6347e7c40710deb41092a8ece83b210a0fb8ecf2
brandonpittman <brandonpittman@gmail.com>
2014-02-10 18:19:34 +0900

    Add better options parsing

--- stockr.rb
+++ stockr.rb
@@ -2,76 +2,94 @@
 
 require 'pathname'
 require 'yaml'
+require 'optparse'
 
-@yaml = Pathname.new('/Users/user/inventory.yml')
+@yaml = Pathname.new("#{Dir.home}/.inventory.yml")
 @stock = YAML.load(@yaml.read)
-@command = ARGV.shift
-@item = ARGV.shift
-if ARGV[0].to_i > 0
-  @amount = ARGV.shift.to_i
-  @link = ARGV.shift
-else
-  @link = ARGV.shift
-end
 
-def new_item
-  unless @stock.include?(@item)
-    @stock[@item] = {total: @amount, quota: 1, link: @link, checked: Time.now}
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: stockr [options]"
+
+  opts.on("-n NAME", "--new NAME", "new NAME") do |name|
+    @item_name = options[:name] = name
   end
+  opts.on("-d", "--delete", "delete") do |delete|
+    @delete = options[:delete] = delete
+  end
+  opts.on("-a AMOUNT", "--add AMOUNT", "add AMOUNT") do |amount|
+    @add = options[:add] = amount.to_i
+  end
+  opts.on("-c", "--check", "check") do |check|
+    @check = options[:check] = check
+  end
+  opts.on("-t AMOUNT", "--total AMOUNT", "total AMOUNT") do |amount|
+    @total = options[:total] = amount.to_i
+  end
+  opts.on("-u URL", "--url URL", "url URL") do |url|
+    @url = options[:url] = url
+  end
+  opts.on("-l AMOUNT", "--limit AMOUNT", "limit AMOUNT") do |limit|
+    @limit = options[:limit] = limit.to_i
+  end
+  opts.on("-b", "--buy", "buy") do |buy|
+    @buy = options[:buy] = buy
+  end
+  opts.on("-h", "--help", "help") do |help|
+    @help = options[:help] = help
+  end
+end.parse!
+
+@stock.each_key { |key| @item = key if key =~ /#{@item_name}/ }
+
+def add
+  @stock[@item_name] = {total: @add, limit: 1, link: @link, checked: Time.now} unless @stock.include?(@item)
 end
 
 def delete
-  if @stock.include?(@item)
-    @stock.delete(@item)
-  end
+  @stock.delete(@item)
 end
 
 def check
   @stock.each do |key, value|
     value["checked"] = Time.now
-    @stock_low = 0
-    if value["total"] <= value["quota"]
-      puts "You need to buy more #{key}!" 
-      @stock_low = @stock_low += 1
+    if value["total"] <= value["limit"]
+      puts "You need to buy more #{key}!"
+      order(key)
     end
   end
-  puts "You're fully stocked!" unless @stock_low > 0
 end
 
 def total
-  puts @stock[@item]["total"]
+  @stock[@item]["total"] = @total
+  time
+end
+
+def time
   @stock[@item]["checked"] = Time.now
-end
-
-def add
-  @stock[@item]["total"] += @amount
-end
-
-def subtract
-  @stock[@item]["total"] -= @amount
 end
 
 def save
   @yaml.write(@stock.to_yaml)
 end
 
-def link
-  @stock[@item]["link"] = @link
+def url
+  @stock[@item]["url"] = @url
+  time
 end
 
-def buy
-  `open -a Safari #{@stock[@item]["link"]}`
+def order(item)
+  `open -a Safari #{@stock[item]["url"]}`
 end
 
-case @command
-  when 'add' then add
-  when 'subtract' then subtract
-  when 'total' then total
-  when 'new' then new_item
-  when 'delete' then delete
-  when 'check' then check
-  when 'buy' then buy
-  when 'link' then link
+def help
+  p "This is the help message." if @help
 end
 
+check if @check
+url if @url
+total if @total
+delete if @delete
+add if @add
+help if @help
 save
