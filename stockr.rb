311daecbc9d656f97a40b762fc9f6e1777c12262
brandonpittman <brandonpittman@gmail.com>
2014-02-12 08:41:41 +0900

    Improve --help messages

--- stockr.rb
+++ stockr.rb
@@ -4,46 +4,47 @@ require 'pathname'
 require 'yaml'
 require 'optparse'
 
-@yaml = Pathname.new("#{Dir.home}/.inventory.yml")
+@yaml = Pathname.new('/Users/brandonpittman/Dropbox/Documents/Code/Ruby/inventory.yml')
 @stock = YAML.load(@yaml.read)
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: stockr [options]"
+  opts.banner = "    Manage an inventory at home or work.
 
-  opts.on("-n NAME", "--new NAME", "new NAME") do |name|
+    Usage: stockr [options]
+
+"
+
+  opts.on("-n NAME", "--new NAME", "Name of inventory item.") do |name|
     @item_name = options[:name] = name
   end
-  opts.on("-d", "--delete", "delete") do |delete|
+  opts.on("-d", "--delete", "Deletes an inventory item.") do |delete|
     @delete = options[:delete] = delete
   end
-  opts.on("-a AMOUNT", "--add AMOUNT", "add AMOUNT") do |amount|
+  opts.on("-a AMOUNT", "--add AMOUNT", "Adds a new inventory item.") do |amount|
     @add = options[:add] = amount.to_i
   end
-  opts.on("-c", "--check", "check") do |check|
+  opts.on("-c", "--check", "Checks for out of stock inventory items.") do |check|
     @check = options[:check] = check
   end
-  opts.on("-t AMOUNT", "--total AMOUNT", "total AMOUNT") do |amount|
+  opts.on("-t AMOUNT", "--total AMOUNT", "Sets total of an inventory item.") do |amount|
     @total = options[:total] = amount.to_i
   end
-  opts.on("-u URL", "--url URL", "url URL") do |url|
+  opts.on("-u URL", "--url URL", "Sets URL of an inventory item.") do |url|
     @url = options[:url] = url
   end
-  opts.on("-l AMOUNT", "--limit AMOUNT", "limit AMOUNT") do |limit|
-    @limit = options[:limit] = limit.to_i
+  opts.on("-m AMOUNT", "--min AMOUNT", "Sets the minimum acceptable amount of an inventory item.") do |min|
+    @min = options[:min] = min.to_i
   end
-  opts.on("-b", "--buy", "buy") do |buy|
+  opts.on("-b", "--buy", "Opens URL of an inventory item.") do |buy|
     @buy = options[:buy] = buy
-  end
-  opts.on("-h", "--help", "help") do |help|
-    @help = options[:help] = help
   end
 end.parse!
 
 @stock.each_key { |key| @item = key if key =~ /#{@item_name}/ }
 
 def add
-  @stock[@item_name] = {total: @add, limit: 1, link: @link, checked: Time.now} unless @stock.include?(@item)
+  @stock[@item_name] = {total: @add, min: 1, url: @url, checked: Time.now} unless @stock.include?(@item)
 end
 
 def delete
@@ -53,7 +54,7 @@ end
 def check
   @stock.each do |key, value|
     value["checked"] = Time.now
-    if value["total"] <= value["limit"]
+    if value["total"] <= value["min"]
       puts "You need to buy more #{key}!"
       order(key)
     end
@@ -82,6 +83,10 @@ def order(item)
   `open -a Safari #{@stock[item]["url"]}`
 end
 
+def min
+  @stock[@item]["min"] = @min
+end
+
 def help
   p "This is the help message." if @help
 end
@@ -91,5 +96,6 @@ url if @url
 total if @total
 delete if @delete
 add if @add
+min if @min
 help if @help
 save
