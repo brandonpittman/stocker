cf183890006d55e0f03b60a1a2c51f1f505f5d4c
brandonpittman <brandonpittman@gmail.com>
2014-02-13 14:51:50 +0900

    Switch to Thor
    stockr now uses Thor to handle commands.

--- stockr.rb
+++ stockr.rb
@@ -2,116 +2,95 @@
 
 require 'pathname'
 require 'yaml'
-require 'optparse'
+require 'thor'
 
-def fallback_path
-  `touch #{Dir.home}/.inventory.yml`
-  @path = "#{Dir.home}/.inventory.yml"
-end
+class Stockr < Thor
+  @@custom_path = '/Users/brandonpittman/Dropbox/Documents/Code/Ruby/inventory.yml'
+  @@path = @@custom_path || fallback_path
+  @@yaml = Pathname.new(@@path)
+  @@stock = YAML.load(@@yaml.read)
 
-@path = '/Users/brandonpittman/Dropbox/Documents/Code/Ruby/inventory.yml' || fallback_path
-@yaml = Pathname.new(@path)
-@stock = YAML.load(@yaml.read)
-
-options = {}
-OptionParser.new do |opts|
-  opts.banner = "    Manage an inventory at home or work.
-
-    Usage: stockr [options]
-
-"
-
-  opts.on("-n NAME", "--name NAME", "Name of inventory item.") do |name|
-    @item_name = options[:name] = name
-  end
-  opts.on("-d", "--delete", "Deletes an inventory item.") do |delete|
-    @delete = options[:delete] = delete
-  end
-  opts.on("-a AMOUNT", "--add AMOUNT", "Adds a new inventory item.") do |amount|
-    @add = options[:add] = amount.to_i
-  end
-  opts.on("-c", "--check", "Checks for out of stock inventory items.") do |check|
-    @check = options[:check] = check
-  end
-  opts.on("-t AMOUNT", "--total AMOUNT", "Sets total of an inventory item.") do |amount|
-    @total = options[:total] = amount.to_i
-  end
-  opts.on("-u URL", "--url URL", "Sets URL of an inventory item.") do |url|
-    @url = options[:url] = url
-  end
-  opts.on("-m AMOUNT", "--min AMOUNT", "Sets the minimum acceptable amount of an inventory item.") do |min|
-    @min = options[:min] = min.to_i
-  end
-  opts.on("-b", "--buy", "Opens URL of an inventory item.") do |buy|
-    @buy = options[:buy] = buy
-  end
-  opts.on("-i", "--inventory", "Take inventory interactively.") { |i| @i = i }
-end.parse!
-
-@stock.each_key { |key| @item = key if key =~ /#{@item_name}/ }
-
-def add
-  @stock[@item_name] = {total: @add, min: 1, url: @url, checked: Time.now} unless @stock.include?(@item)
-end
-
-def delete
-  @stock.delete(@item)
-end
-
-def check
-  @stock.each do |key, value|
-    value["checked"] = Time.now
-    if value["total"] <= value["min"]
-      puts "You need to buy more #{key}!"
-      order(key)
+  no_commands do
+    def self.fallback_path
+    `touch #{Dir.home}/.inventory.yml`
+    @@path = "#{Dir.home}/.inventory.yml"
     end
   end
-end
 
-def total
-  @stock[@item]["total"] = @total
-  time
-end
+  desc "add ITEM", "Add ITEM to inventory."
+  option :url, :aliases => :u, :required => true
+  option :total, :aliases => :t, :required => true
+  option :min, :aliases => :m, :default => 1
+  def add(item)
+    @@stock[item] = {'total' => options[:total].to_i, 'min' => options[:min].to_i, 'url' => options[:url], 'checked' => Time.now}
+    Stockr.save
+  end
 
-def time
-  @stock[@item]["checked"] = Time.now
-end
+  desc "delete ITEM", "Delete ITEM from inventory."
+  def delete(item)
+    @@stock.each_key { |key| @item_name = key if key =~ /#{item}/ }
+    @@stock.delete(@item_name)
+    Stockr.save
+  end
 
-def save
-  @yaml.write(@stock.to_yaml)
-end
+  desc "check", "Check for low stock items."
+  def check
+    @@stock.each do |key, value|
+      value["checked"] = Time.now
+      if value["total"] <= value["min"]
+        puts "You need to buy more #{key}!"
+        buy(key)
+      end
+    end
+    Stockr.save
+  end
 
-def url
-  @stock[@item]["url"] = @url
-  time
-end
+  desc "total ITEM TOTAL", "Set TOTAL of ITEM."
+  def total(item, total)
+    @@stock[item]["total"] = total.to_i
+    Stockr.time(item)
+    Stockr.save
+  end
 
-def order(item)
-  `open -a Safari #{@stock[item]["url"]}`
-end
+  no_commands do
+    def self.time(item)
+      @@stock[item]["checked"] = Time.now
+    end
+  end
 
-def min
-  @stock[@item]["min"] = @min
-end
+  no_commands do
+    def self.save
+      @@yaml.write(@@stock.to_yaml)
+    end
+  end
 
-def help
-  p "This is the help message." if @help
-end
+  desc "url ITEM URL", "Set URL of ITEM."
+  def url(item, url)
+    @@stock[item]["url"] = url
+    Stockr.time(item)
+    Stockr.save
+  end
 
-def inventory
-  @stock.each do |key, value|
-    value["checked"] = Time.now
-    puts "Enter total for #{key}:"
-    value["total"] = gets.chomp.to_i
+  desc "buy ITEM", "Open link to buy ITEM"
+  def buy(item)
+    `open -a Safari #{@@stock[item]["url"]}`
+  end
+
+  desc "min ITEM MINIMUM", "Set minimum acceptable amount for ITEM."
+  def min(item, min)
+    @@stock[item]["min"] = min.to_i
+    Stockr.save
+  end
+
+  desc "count", "Take an interactive inventory."
+  def count
+    @@stock.each do |key, value|
+      value["checked"] = Time.now
+      puts "Enter total for #{key}:"
+      value["total"] = gets.chomp.to_i
+    end
+    Stockr.save
   end
 end
 
-check if @check
-url if @url
-total if @total
-delete if @delete
-add if @add
-min if @min
-help if @help
-inventory if @i
-save
+Stockr.start(ARGV)
